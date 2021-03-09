f7a09a19e37b0ac72024d16faf6f249d1a30e598
Zizu <angelzizu06@gmail.com>
2021-03-09 11:01:21 -0500

    components and version beta

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import App from './containers/App'
 
 ReactDOM.render( 
-<HelloWorld/>,
+<App/>,
 document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,15 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {name: 'assets/[hash].[ext]'},
+                    }
+                ]
             }
         ]
     },
